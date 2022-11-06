a2ff56e27e74a284cdd74cd7de0f37163b05b9ed
조정원 <whwjddnjs8@gmail.com>
2022-11-06 19:23:30 +0900

    반응형 작업 전

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ $(function () {
         $('.sub-menu-area').stop().slideUp()
     })
 
-
-	var mySwiper = new Swiper('.swiper-container', {
+    /* Slide Area */
+    var mySwiper = new Swiper('.swiper-container', {
             slidesPerView: 1,
             slidesPerGroup: 1,
             spaceBetween: 30,
@@ -35,5 +35,57 @@ $(function () {
             }
 
             document.getElementById(tabName).style.display = "block";
+    }
+    /* Fast Menu Slide Area */
+    $('.slide_next').click(function () {
+        var left = $(this).siblings('div').children().css('left')
+        
+        if (left == '0px') {
+            $('.content_area').css({"left" : "-456px"})
+        } else if (left == '-456px') {
+             $('.content_area').css({"left" : "-912px"})
+        } else if (left == '-912px') {
+             $('.content_area').css({"left" : "0"})
         }
+    })
+
+    $('.slide_prev').click(function () {
+        var left = $(this).siblings('div').children().css('left')
+        
+        if (left == '0px') {
+            $('.content_area').css({"left" : "-912px"})
+        } else if (left == '-456px') {
+             $('.content_area').css({"left" : "0"})
+        } else if (left == '-912px') {
+             $('.content_area').css({"left" : "-456px"})
+        }
+    })
+
+    /* Tab Area */
+    $('.class_area').hide()
+    $('.media_area').hide()
+    $('.tab_area .btn_area ul li').click(function () {
+        var idx = $(this).index();  
+
+        $(this).siblings().removeClass('active')
+        $(this).addClass('active')
+        
+        
+        if (idx == 0) {
+            $('.news_area').show()
+            $('.class_area').hide()
+            $('.media_area').hide()
+        } else if (idx == 1) {
+            $('.news_area').hide()
+            $('.class_area').show()
+            $('.media_area').hide()
+        } else if (idx == 2) {
+            $('.news_area').hide()
+            $('.class_area').hide()
+            $('.media_area').show()
+        }
+    })
+
+
+	
 })
